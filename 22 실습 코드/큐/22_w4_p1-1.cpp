308022f7a40d56c75ed911146d4cbefabccefdd1
임주민 <limj1213@gmail.com>
2023-03-20 20:25:21 +0900

    Remove some code comments on 22 queue files.

--- 22 실습 코드/큐/22_w4_p1-1.cpp
+++ 22 실습 코드/큐/22_w4_p1-1.cpp
@@ -1,104 +1,104 @@
-////
-////  22_w4_p1_1.cpp
-////  LandVibeQueue
-////
-////  Created by 임주민 on 2023/03/20.
-////
 //
-///*
-// 22년도 4주차 문제 1-1번
-// 링리를 통한 큐 구현
-// */
+//  22_w4_p1_1.cpp
+//  LandVibeQueue
 //
-//#include <iostream>
-//using namespace std;
+//  Created by 임주민 on 2023/03/20.
 //
-//int s; // (큐가 담을 수 있는 원소 개수)클래스 내에서도 사용해 줄거라서 전역변수로 선언
-//
-//class Node {
-//public:
-//    Node *next = nullptr;
-//    int value;
-//};
-//
-//class Queue {
-//public:
-//    Node* head = nullptr;
-//    Node* tail = nullptr;
-//    int size = 0;
-//    
-//    int getSize() { return size; }
-//    bool isEmpty() { return size==0; }
-//    void front() {
-//        if (isEmpty()) { // 문제 조건 따라 비어있는 경우 에러처리
-//            cout << "Empty\n";
-//        } else {
-//            cout << head->value << "\n";
-//        }
-//    }
-//    void rear() {
-//        if (isEmpty()) { // 문제 조건 따라 비어있는 경우 에러처리
-//            cout << "Empty\n";
-//        } else {
-//            cout << tail->value << "\n";
-//        }
-//    }
-//    void enpueue(int value) { // tail에만 추가하는 경우
-//        if (size >= s) {
-//            cout << "Full\n";
-//        } else {
-//            Node* newNode = new Node;
-//            newNode->value = value; // 새 노드 생성 후 값 할당
-//            
-//            if (isEmpty()) { // 빈 경우
-//                head = tail = newNode; // head와 tail 모두 그 노드로 설정
-//            } else { // 노드가 있었다면
-//                tail->next = newNode; // 기존 tail 뒤에 새 노드 연결
-//                tail = newNode; // tail 업데이트
-//            }
-//            
-//            size++; // 사이즈 증가
-//        }
-//    }
-//    void dequeue() { // head만 삭제하는 경우
-//        if (isEmpty()) { // 문제 조건 따라 비어있는 경우 에러처리
-//            cout << "Empty\n";
-//        } else {
-//            Node* deleteNode = head; // 삭제할 노드 지정 후 삭제할 값 출력
-//            cout << deleteNode->value << "\n"; // 또는 head->value
-//            
-//            if (size==1) { // 삭제하면 비게될 경우
-//                head = tail = nullptr; // head와 tail 모두 아무것도 안가리키게 설정
-//            } else { // 삭제해도 노드 존재할 경우 head 업데이트
-//                head = deleteNode->next; //또는 head->next
-//            }
-//    
-//            delete deleteNode; // head 삭제하는거 X
-//            size--; // delete 후 사이즈 감소
-//        }
-//    }
-//};
-//
-//int main() {
-//    cin >> s;
-//    Queue q;
-//    int numOfCmd; cin >> numOfCmd;
-//    while(numOfCmd--) {
-//        string cmd; cin >> cmd;
-//        if (cmd == "enqueue") {
-//            int value; cin >> value;
-//            q.enpueue(value);
-//        } else if (cmd == "dequeue") {
-//            q.dequeue();
-//        } else if (cmd == "isEmpty") {
-//            cout << q.isEmpty() << "\n";
-//        } else if (cmd == "size") {
-//            cout << q.getSize() << "\n";
-//        } else if (cmd == "front") {
-//            q.front();
-//        } else if (cmd == "rear") {
-//            q.rear();
-//        }
-//    }
-//}
-//
+
+/*
+ 22년도 4주차 문제 1-1번
+ 링리를 통한 큐 구현
+ */
+
+#include <iostream>
+using namespace std;
+
+int s; // (큐가 담을 수 있는 원소 개수)클래스 내에서도 사용해 줄거라서 전역변수로 선언
+
+class Node {
+public:
+    Node *next = nullptr;
+    int value;
+};
+
+class Queue {
+public:
+    Node* head = nullptr;
+    Node* tail = nullptr;
+    int size = 0;
+    
+    int getSize() { return size; }
+    bool isEmpty() { return size==0; }
+    void front() {
+        if (isEmpty()) { // 문제 조건 따라 비어있는 경우 에러처리
+            cout << "Empty\n";
+        } else {
+            cout << head->value << "\n";
+        }
+    }
+    void rear() {
+        if (isEmpty()) { // 문제 조건 따라 비어있는 경우 에러처리
+            cout << "Empty\n";
+        } else {
+            cout << tail->value << "\n";
+        }
+    }
+    void enpueue(int value) { // tail에만 추가하는 경우
+        if (size >= s) {
+            cout << "Full\n";
+        } else {
+            Node* newNode = new Node;
+            newNode->value = value; // 새 노드 생성 후 값 할당
+            
+            if (isEmpty()) { // 빈 경우
+                head = tail = newNode; // head와 tail 모두 그 노드로 설정
+            } else { // 노드가 있었다면
+                tail->next = newNode; // 기존 tail 뒤에 새 노드 연결
+                tail = newNode; // tail 업데이트
+            }
+            
+            size++; // 사이즈 증가
+        }
+    }
+    void dequeue() { // head만 삭제하는 경우
+        if (isEmpty()) { // 문제 조건 따라 비어있는 경우 에러처리
+            cout << "Empty\n";
+        } else {
+            Node* deleteNode = head; // 삭제할 노드 지정 후 삭제할 값 출력
+            cout << deleteNode->value << "\n"; // 또는 head->value
+            
+            if (size==1) { // 삭제하면 비게될 경우
+                head = tail = nullptr; // head와 tail 모두 아무것도 안가리키게 설정
+            } else { // 삭제해도 노드 존재할 경우 head 업데이트
+                head = deleteNode->next; //또는 head->next
+            }
+    
+            delete deleteNode; // head 삭제하는거 X
+            size--; // delete 후 사이즈 감소
+        }
+    }
+};
+
+int main() {
+    cin >> s;
+    Queue q;
+    int numOfCmd; cin >> numOfCmd;
+    while(numOfCmd--) {
+        string cmd; cin >> cmd;
+        if (cmd == "enqueue") {
+            int value; cin >> value;
+            q.enpueue(value);
+        } else if (cmd == "dequeue") {
+            q.dequeue();
+        } else if (cmd == "isEmpty") {
+            cout << q.isEmpty() << "\n";
+        } else if (cmd == "size") {
+            cout << q.getSize() << "\n";
+        } else if (cmd == "front") {
+            q.front();
+        } else if (cmd == "rear") {
+            q.rear();
+        }
+    }
+}
+
--- 22 실습 코드/큐/22_w4_p1-2.cpp
+++ 22 실습 코드/큐/22_w4_p1-2.cpp
@@ -1,87 +1,87 @@
-////
-////  22_w4_p1_2.cpp
-////  LandVibeQueue
-////
-////  Created by 임주민 on 2023/03/20.
-////
 //
-///*
-// 22년도 4주차 문제 1-2번
-// 배열을 통한 큐 구현
-// */
+//  22_w4_p1_2.cpp
+//  LandVibeQueue
 //
-//#include <iostream>
-//using namespace std;
+//  Created by 임주민 on 2023/03/20.
 //
-//int n; // 입력받을 큐 Q의 크기를 전역변수로 선언(클래스 내에서도 사용해줄 거라서)
-//
-//class Queue {
-//public:
-//    int arr[10000]; // 최대 크기 만큼 배열 선언
-//    int frontIdx = 0; // 데이터가 들어올 인덱스 0으로 초기화
-//    int rearIdx = -1; // 데이터가 나갈 인덱스 -1로 초기화(아무것도 없을 떄)
-//    int size = 0; // 사이즈 0으로 초기화
-//
-//    void printSize() { cout << size << "\n"; }
-//    bool isEmpty() { return size == 0; }
-//    void updateIdx() { // *배열을 통한 큐 구현의 중요한 부분
-//        frontIdx %= n;
-//        rearIdx %= n;
-//    }
-//    void front() {
-//        if (isEmpty()) cout << "Empty\n";
-//        else {
-//            cout << arr[frontIdx] << "\n"; // 해당 인덱스 내의 데이터 그대로 출력
-//        }
-//    }
-//    void rear() {
-//        if (isEmpty()) cout << "Empty\n";
-//        else {
-//            cout << arr[rearIdx] << "\n"; // 해당 인덱스 내의 데이터 그대로 출력
-//        }
-//    }
-//    void enqueue(int value) { // 삽입
-//        if (size >= n) {
-//            cout << "Full\n";
-//        } else {
-//            rearIdx++; // rear을 나타내는 인덱스 증감 후 업데이트함수 실행(배열 최대 인덱스 넘어가면 안되니까)
-//            updateIdx();
-//            arr[rearIdx] = value; // 업데이트 된 rearIdx에 데이터 할당
-//            size++; // 사이즈 증가
-//        }
-//    }
-//    void dequeue() { // 삭제
-//        if (isEmpty()) {
-//            cout << "Empty\n";
-//        } else {
-//            cout << arr[frontIdx] << "\n"; // 삭제될 인덱스의 데이터 출력
-//            frontIdx++; // frontIdx 증감 후 업데이트 함수 실행(배열 최대 인덱스 넘어가면 안되니까)
-//            updateIdx();
-//            size--; // 사이즈 감소
-//        }
-//    }
-//
-//};
-//
-//int main() {
-//    int t;
-//    cin >> n >> t;
-//    Queue q;
-//    while(t--) {
-//        string cmd; cin >> cmd;
-//        if (cmd == "enqueue") {
-//            int value; cin >> value;
-//            q.enqueue(value);
-//        } else if (cmd=="dequeue") {
-//            q.dequeue();
-//        } else if (cmd=="isEmpty") {
-//            cout << q.isEmpty() << "\n";
-//        } else if (cmd=="size") {
-//            q.printSize();
-//        } else if (cmd=="front") {
-//            q.front();
-//        } else if (cmd=="rear") {
-//            q.rear();
-//        }
-//    }
-//}
+
+/*
+ 22년도 4주차 문제 1-2번
+ 배열을 통한 큐 구현
+ */
+
+#include <iostream>
+using namespace std;
+
+int n; // 입력받을 큐 Q의 크기를 전역변수로 선언(클래스 내에서도 사용해줄 거라서)
+
+class Queue {
+public:
+    int arr[10000]; // 최대 크기 만큼 배열 선언
+    int frontIdx = 0; // 데이터가 들어올 인덱스 0으로 초기화
+    int rearIdx = -1; // 데이터가 나갈 인덱스 -1로 초기화(아무것도 없을 떄)
+    int size = 0; // 사이즈 0으로 초기화
+
+    void printSize() { cout << size << "\n"; }
+    bool isEmpty() { return size == 0; }
+    void updateIdx() { // *배열을 통한 큐 구현의 중요한 부분
+        frontIdx %= n;
+        rearIdx %= n;
+    }
+    void front() {
+        if (isEmpty()) cout << "Empty\n";
+        else {
+            cout << arr[frontIdx] << "\n"; // 해당 인덱스 내의 데이터 그대로 출력
+        }
+    }
+    void rear() {
+        if (isEmpty()) cout << "Empty\n";
+        else {
+            cout << arr[rearIdx] << "\n"; // 해당 인덱스 내의 데이터 그대로 출력
+        }
+    }
+    void enqueue(int value) { // 삽입
+        if (size >= n) {
+            cout << "Full\n";
+        } else {
+            rearIdx++; // rear을 나타내는 인덱스 증감 후 업데이트함수 실행(배열 최대 인덱스 넘어가면 안되니까)
+            updateIdx();
+            arr[rearIdx] = value; // 업데이트 된 rearIdx에 데이터 할당
+            size++; // 사이즈 증가
+        }
+    }
+    void dequeue() { // 삭제
+        if (isEmpty()) {
+            cout << "Empty\n";
+        } else {
+            cout << arr[frontIdx] << "\n"; // 삭제될 인덱스의 데이터 출력
+            frontIdx++; // frontIdx 증감 후 업데이트 함수 실행(배열 최대 인덱스 넘어가면 안되니까)
+            updateIdx();
+            size--; // 사이즈 감소
+        }
+    }
+
+};
+
+int main() {
+    int t;
+    cin >> n >> t;
+    Queue q;
+    while(t--) {
+        string cmd; cin >> cmd;
+        if (cmd == "enqueue") {
+            int value; cin >> value;
+            q.enqueue(value);
+        } else if (cmd=="dequeue") {
+            q.dequeue();
+        } else if (cmd=="isEmpty") {
+            cout << q.isEmpty() << "\n";
+        } else if (cmd=="size") {
+            q.printSize();
+        } else if (cmd=="front") {
+            q.front();
+        } else if (cmd=="rear") {
+            q.rear();
+        }
+    }
+}
